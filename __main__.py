790b5940d0a2e3b00c03f74d5385dedc117c2df8
David Ellinger <david.ellinger@me.com>
2022-06-09 21:27:00 -0400

    Add todoist integration

---
+++ __main__.py
@@ -0,0 +1,7 @@
+from app import app
+import logging
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    app.run()
---
+++ app/app.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from dotenv import load_dotenv
+from todoist_api_python.api import TodoistAPI
+
+load_dotenv()
+logger = logging.getLogger(__name__)
+TODOIST_API_KEY = os.getenv("TODOIST_API_KEY")
+
+def get_tasks():
+    api = TodoistAPI(TODOIST_API_KEY)
+    try:
+        tasks = api.get_tasks()
+        [logger.info(f"Task: {task.content} | Created: {task.created}") for task in tasks]
+    except Exception as error:
+        logger.exception("Error retrieving tasks")
+
+def run():
+    logger.info("Hello World")
+    get_tasks()
